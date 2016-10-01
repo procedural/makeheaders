7f6ca9d91fbd8e0745efd4accee34e3e933816e6
Constantine Tarasenkov <constantine@protonmail.com>
2016-10-01 05:25:19 +0300

    Enable optimizations

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname -- "$(readlink -fn -- "${0}")")"
 
-clang -o makeheaders makeheaders.c -static -nostdinc -nostdlib -I/usr/local/musl/include/ -L/usr/local/musl/lib/ /usr/local/musl/lib/crt1.o /usr/local/musl/lib/crti.o /usr/local/musl/lib/crtn.o -lc -g -DDEBUG
+clang -o makeheaders makeheaders.c -static -nostdinc -nostdlib -I/usr/local/musl/include/ -L/usr/local/musl/lib/ /usr/local/musl/lib/crt1.o /usr/local/musl/lib/crti.o /usr/local/musl/lib/crtn.o -lc -O3
